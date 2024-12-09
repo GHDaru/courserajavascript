1a5d1e022a31510f7cd37e593813cf97f1da7373
GHDaru <ghdaru@gmail.com>
2024-12-08 20:51:21 -0500

    final

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -5,6 +5,7 @@ function submitFeedback() {
     const job = document.getElementById('job').value;
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
+    const Experience = document.getElementById('Experience').value;
     const feedback = document.getElementById('feedbackText').value;
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -13,6 +14,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = Experience;
     document.getElementById('userInfo').style.display = 'block';
     alert('Thank you for your valuable feedback')
 }
